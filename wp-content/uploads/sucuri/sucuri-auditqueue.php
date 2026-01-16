9e2fc12c7668f6752f968d0f8b2257b96cc81b4c
Jyoti <jyoti@maven.one>
2026-01-16 12:56:10 +0530

    homepage setup and demo screenshot update in readme

--- wp-content/uploads/sucuri/sucuri-auditqueue.php
+++ wp-content/uploads/sucuri/sucuri-auditqueue.php
@@ -5,3 +5,13 @@
 exit(0);
 ?>
 1768545231_4203:"Warning: contactpointdemo, 127.0.0.1; Plugin activated: Sucuri Security - Auditing, Malware Scanner and Hardening (v2.6; sucuri-scanner\/sucuri.php)"
+1768546570_2129:"Notice: contactpointdemo, 127.0.0.1; Wpcf7_contact_form status has been changed; details: ID: 79,Old status: new,New status: publish,Title: Inquiry Form"
+1768546602_679:"Notice: contactpointdemo, 127.0.0.1; Custom_css status has been changed; details: ID: 81,Old status: new,New status: publish,Title: hello-child"
+1768546604_76:"Notice: contactpointdemo, 127.0.0.1; Revision status has been changed; details: ID: 82,Old status: new,New status: inherit,Title: hello-child"
+1768546606_8365:"Warning: contactpointdemo, 127.0.0.1; Post moved to trash; ID: 80; name: ; status: publish"
+1768546606_8457:"Notice: contactpointdemo, 127.0.0.1; Customize_changeset status has been changed; details: ID: 80,Old status: publish,New status: trash,Title: "
+1768546608_927:"Notice: contactpointdemo, 127.0.0.1; Customize_changeset status has been changed; details: ID: 80,Old status: new,New status: publish,Title: "
+1768547034_3203:"Notice: contactpointdemo, 127.0.0.1; Page status has been changed; details: ID: 89,Old status: new,New status: auto-draft,Title: Auto Draft"
+1768547045_7939:"Notice: contactpointdemo, 127.0.0.1; Page status has been changed; details: ID: 89,Old status: auto-draft,New status: publish,Title: Contact Us"
+1768547045_9348:"Notice: contactpointdemo, 127.0.0.1; Page was created; ID: 89; name: Contact Us"
+1768547045_9476:"Notice: contactpointdemo, 127.0.0.1; Revision status has been changed; details: ID: 90,Old status: new,New status: inherit,Title: Contact Us"
